4e2d9df50a4e084aeca88f808e82d00a9938c438
martijnvanlier <98356376+martijnvanlier@users.noreply.github.com>
2022-01-26 15:42:58 +0100

    Update House_prices_Train_Test_Predict.py

--- House_prices_Train_Test_Predict.py
+++ House_prices_Train_Test_Predict.py
@@ -1,56 +1,125 @@
+#Import required packages
+import numpy as np
 import pandas as pd
 import sklearn
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import make_regression
+from sklearn import metrics
+import xgboost as xgb
 
-#Import training data from csv file
-train = pd.read_csv('D:/Kaggle/House Prices/house-prices-advanced-regression-techniques/train.csv')
-test = pd.read_csv('D:/Kaggle/House Prices/house-prices-advanced-regression-techniques/test.csv')
-sample_submission = pd.read_csv('D:/Kaggle/House Prices/house-prices-advanced-regression-techniques/sample_submission.csv')
+#1) DATA PRE-PROCESSING
 
-#Store ID in index (because it doesn't have any predictive power)
+#1.1) Import train and test data from csv file
+train = pd.read_csv('/home/ec2-user/environment/Kaggle/train.csv')
+test = pd.read_csv('/home/ec2-user/environment/Kaggle/test.csv')
+sample_submission = pd.read_csv('/home/ec2-user/environment/Kaggle/sample_submission.csv')
+
+#1.2) Store ID in index (because it doesn't have any predictive power)
 train.set_index('Id', inplace=True)
 test.set_index('Id', inplace=True)
 
-#1) Store X and y variables from training data separate
+#1.3) Store X and y variables from training data separate
 y = train['SalePrice']
-X=train.drop(['SalePrice'], axis=1) #Drop SalePrice (because it's Y)
+X=train.drop(['SalePrice'], axis=1) #Drop SalePrice (because it's the variable to predict).
 
-#2) One-hot-encoding
-ohe= OneHotEncoder(handle_unknown='ignore', sparse=False)
-ohe.fit(X)
-X_ohe=ohe.transform(X)
+#1.4) One-hot-encoding, convert categories to dummies
+train = pd.get_dummies(X)   
+test = pd.get_dummies(test)     
+final_train, final_test = train.align(test, join='inner', axis=1)  #Include only columns in test set which are available in training set.
 
-#3) Check for NAs in dataset
-np.any(np.isnan(X))
-np.all(np.isfinite(X))
+#Check if number of columns aligns
+print(final_train.shape)
+print(final_test.shape)
 
-#4) Fill NAs with mean for now (otherwise model generation will give errors)
-#X.fillna(X.mean(), inplace=True)
+#1.5) Fill NAs with mean and verify results
+final_train.fillna(final_train.mean(), inplace=True)
+final_test.fillna(final_test.mean(), inplace=True)
 
-#5) split into train test sets
-X_train, X_test, y_train, y_test = train_test_split(X_ohe, y, train_size=0.70)
+print(np.any(np.isnan(final_train)))
+print(np.any(np.isnan(final_test)))
 
-#6) Train linear regression model
-reg = LinearRegression().fit(X_train, y_train)
-reg.score(X_train, y_train)
+#2) TRAIN AND COMPARE MODELS
 
-#7) Test linear regression model
-reg.score(X_test, y_test)
+#2.1) Split into train test sets
+X_train, X_test, y_train, y_test = train_test_split(final_train, y, train_size=0.70)
 
-#8) Create predictions for test set
+#2.2) Train and compare models
 
-#8.1)Data preparation
-test_cleaned=ohe.transform(test)
-test_cleaned.fillna(test_cleaned.mean(), inplace=True)
-#8.2)Predictions
-predictions = reg.predict(test_cleaned)
-#8.3) Convert results from array to dataframe
+#2.2.1) linear regression model
+regression = LinearRegression().fit(X_train, y_train)
+
+#Check score
+print(regression.score(X_train, y_train))
+print(regression.score(X_test, y_test))
+
+#Use Root Mean Squared Log Error (RMSLE) as evaluation metric
+y_train_pred = regression.predict(X_train)
+y_test_pred = regression.predict(X_test)
+
+print(metrics.mean_squared_log_error(y_train, y_train_pred, squared=False))
+#print(metrics.mean_squared_log_error(y_test, y_test_pred, squared=False)) #MMean Squared Logarithmic Error cannot be used when targets contain negative values.
+
+#2.2.2) Random forest
+randomforest = RandomForestRegressor()
+randomforest.fit(X_train, y_train)
+y_train_pred = randomforest.predict(X_train)
+y_test_pred = randomforest.predict(X_test)
+
+#Evaluate model on train/test
+print(metrics.mean_squared_log_error(y_train, y_train_pred, squared=False))
+print(metrics.mean_squared_log_error(y_test, y_test_pred, squared=False))
+
+#2.2.3) Xgboost
+xgboost = xgb.XGBRegressor()
+xgboost = xgboost.fit(X_train, y_train)
+
+y_train_pred = xgboost.predict(X_train)
+y_test_pred = xgboost.predict(X_test)
+
+#Evaluate model on train/test
+print(metrics.mean_squared_log_error(y_train, y_train_pred, squared=False))
+print(metrics.mean_squared_log_error(y_test, y_test_pred, squared=False))
+
+#2.3) List most important features and export
+feature_importance = pd.DataFrame(xgboost.feature_importances_,
+                                   index = X_train.columns,
+                                   columns=['importance']).sort_values('importance',                                                                 
+                                    ascending=False)
+
+feature_importance.to_csv('/home/ec2-user/environment/Kaggle/26_01_2022_feature_importance.csv', index = True)
+
+#2.4) Retrain with 150 most important features
+feature_importance.reset_index(level=0, inplace=True)
+feature_importance.columns = ['feature','importance']
+top_features = feature_importance[feature_importance['importance'] > 0.0001]
+features_list = top_features['feature'].tolist()
+final_train = train[train.columns.intersection(features_list)]
+final_test = test[test.columns.intersection(features_list)]
+
+X_train, X_test, y_train, y_test = train_test_split(final_train, y, train_size=0.70)
+xgboost = xgb.XGBRegressor()
+xgboost = xgboost.fit(X_train, y_train)
+y_train_pred = xgboost.predict(X_train)
+y_test_pred = xgboost.predict(X_test)
+print(metrics.mean_squared_log_error(y_train, y_train_pred, squared=False))
+print(metrics.mean_squared_log_error(y_test, y_test_pred, squared=False))
+
+#3) CREATE PREDICTIONS
+
+#3.1) Make predictions based on xgboost
+predictions = xgboost.predict(final_test)
+
+#3.2) Convert results from array to dataframe
 predictions = pd.DataFrame(predictions)
-#8.4) Give column a name
+
+#3.3) Give column a name
 predictions.columns = ['SalePrice']
-#8.5) Put predictions in sample_submission
+
+#3.4) Put predictions in sample_submission
 sample_submission['SalePrice'] = predictions
-#8.6) Store results in csv
-sample_submission.to_csv('D:/Kaggle/House Prices/submission_10_01_2022.csv', index = False)
+
+#3.5) Store results in csv
+sample_submission.to_csv('/home/ec2-user/environment/Kaggle/26_01_2022_submission.csv', index = False)
